9a454fd8e041e4e6af427564d362e9c47a3af196
greffgreff <greffchandler80@gmail.com>
2023-03-21 13:31:38 +0100

    Separated identity client into contact and registation

--- src/backend/clients/registrationClient.ts
+++ src/backend/clients/registrationClient.ts
@@ -6,7 +6,6 @@ import { AxiosError } from 'axios'
 
 const baseRegister = 'https://localhost:44354/users/register/'
 const baseUnregister = 'https://localhost:44354/users/unregister/'
-const baseContact = 'https://localhost:44354/contact/'
 
 export async function register(user: BasicUser): Promise<User> {
   try {
@@ -18,7 +17,7 @@ export async function register(user: BasicUser): Promise<User> {
         case 400:
           throw Error(`User with the same name already exists.`)
         case 401:
-          throw Error(`Client is not authorised to register new users.`) // handle
+          throw Error(`Client is not authorised to register new users.`)
         case 500:
           throw Error('Could not register user as there was an internal server error.')
         default:
@@ -32,28 +31,6 @@ export async function register(user: BasicUser): Promise<User> {
 
 export async function unregister(user: User): Promise<void> {
   await axios.delete(baseUnregister + user.id)
-}
-
-export async function contact(username: string): Promise<User> {
-  try {
-    const res = await axios.get(baseContact + encodeURIComponent(username), headers())
-    return res.data
-  } catch (error) {
-    if (error instanceof AxiosError) {
-      switch (error?.response?.status) {
-        case 404:
-          throw Error(`User not found.`)
-        case 401:
-          throw Error(`Client is not authorised to contact users.`) // handle
-        case 500:
-          throw Error('Could not contact user as there was an internal server error.')
-        default:
-          throw Error('Could not contact user.')
-      }
-    } else {
-      throw Error('Could not connect to contacting services')
-    }
-  }
 }
 
 function headers() {
