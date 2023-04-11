8ffc3bb030f17d6fc142413ff4e5c5fea709f73f
greffgreff <greffchandler80@gmail.com>
2023-04-11 19:09:49 +0200

    Cleaned up ipc

--- src/renderer/global.d.ts
+++ src/renderer/global.d.ts
@@ -1,20 +1,14 @@
 import { Credentials, Registration, User, Message, Channel } from '../common/types'
-import { IpcRendererEvent } from 'electron'
 
 export declare global {
 	interface Window {
 		electron: {
-			onOnline: (callback: (event: IpcRendererEvent, isOnline: boolean) => void) => void
-			isOnline: () => Promise<boolean>
-			sendMessage: (recipientJid: string, message: Message) => void
-			onMessageReceive: (callback: (event: IpcRendererEvent, message: Message) => void) => void
-			createChannel: (channel: Channel) => Promise<Channel>
-			getChannels: () => Promise<Channel[]>
-			getChannelConversation: (channelId: string) => Promise<Message[]>
-			contactUser: (username: string) => Promise<User | string>
-			
+			getAppData: <T>(key: string) => Promise<T>
+			setAppData: (key: string, value: any) => Promise<void>
+			onAppDataChange: (callback: (key: string, value: any) => void) => void
+
 			onError: (callback: (type: string, message: string) => void) => void
-			
+
 			register: (registration: Registration) => Promise<void>
 			login: (credentials: Credentials) => Promise<void>
 			logout: () => void
@@ -22,9 +16,11 @@ export declare global {
 			getAuthState: () => Promise<'notRegistered' | 'loggedOut' | 'loggedIn'>
 			onAuthStateChange: (callback: (state: 'notRegistered' | 'loggedOut' | 'loggedIn') => void) => void
 
-			getAppData: <T>(key: string) => Promise<T>
-			setAppData: (key: string, value: any) => Promise<void>
-			onAppDataChange: (callback: (key: string, value: any) => void) => void
+			onOnline: (callback: (isOnline: boolean) => void) => void
+			isOnline: () => Promise<boolean>
+			
+			sendMessage: (recipientJid: string, message: Message) => Promise<void>
+			onMessageReceive: (callback: (message: Message) => void) => void
 		}
 	}
 }
