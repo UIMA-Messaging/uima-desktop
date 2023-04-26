7551774ead0552093e57da70e8847730ef814efb
greffgreff <greffchandler80@gmail.com>
2023-04-26 14:14:20 +0200

    Made request to fetch key bundle on contact create

--- src/main/clients/identity-client.ts
+++ src/main/clients/identity-client.ts
@@ -1,29 +1,30 @@
 import axios, { AxiosError } from 'axios'
 import https from 'https'
 import isDev from 'electron-is-dev'
-import { ExchangeKeys } from '../../common/types/SigalProtocol'
+import { KeyBundle } from '../../common/types/SigalProtocol'
 
-const registerExchangeKeysBaseUrl = 'https://localhost:44317/keys/register/exchanges'
+const registerExchangeKeysBaseUrl = 'https://localhost:44317/keys/bundle/'
 
-// export async function registerExchangeKey(exchangeKeys: ExchangeKeys): Promise<void> {
-// 	try {
-// 		await axios.post(registerExchangeKeysBaseUrl, exchangeKeys, headers())
-// 	} catch (error) {
-// 		if (error instanceof AxiosError) {
-// 			switch (error?.response?.status) {
-// 				case 401:
-// 					throw Error(`Client is not authorised to register keys.`)
-// 				case 500:
-// 					throw Error('Could not register keys as there was an internal server error.')
-// 				default:
-// 					console.log(JSON.stringify(error))
-// 					throw Error('Could not register keys.')
-// 			}
-// 		} else {
-// 			throw Error('Could not connect to identity services')
-// 		}
-// 	}
-// }
+export async function getKeyBundleForUser(from: string, to: string): Promise<KeyBundle> {
+	try {
+		const res = await axios.post(registerExchangeKeysBaseUrl + `${from}/${to}`, headers())
+		return res.data
+	} catch (error) {
+		if (error instanceof AxiosError) {
+			switch (error?.response?.status) {
+				case 401:
+					throw Error(`Client is not authorised to fetch key bundle.`)
+				case 500:
+					throw Error('Could not fetch key bundle as there was an internal server error.')
+				default:
+					console.log(JSON.stringify(error))
+					throw Error('Could not fetch key bundle.')
+			}
+		} else {
+			throw Error('Could not connect to identity services')
+		}
+	}
+}
 
 function headers() {
 	return isDev ? { httpsAgent: new https.Agent({ rejectUnauthorized: false }) } : null
