cfa861700481de1a7ad06c39f262750da0e8eb6f
greffgreff <greffchandler80@gmail.com>
2023-05-03 14:27:36 +0200

    Fixed time wrap with long messages

--- src/renderer/components/ChatBubble.tsx
+++ src/renderer/components/ChatBubble.tsx
@@ -5,13 +5,13 @@ import { wordToHexColor } from '../utils/colors'
 export default ({ text, author, timestamp }: { text: string; author: string; timestamp: any }) => {
 	return (
 		<div className="chat-bubble-container">
-			<div className="chat-bubble-message">
-				<span className="message-author" style={{ color: wordToHexColor(author) }}>
-					{author}
-				</span>
-				<span className="message-text">{text}</span>
-			</div>
-			<span className="message-time">{getNaturalTimeFromDate(new Date(timestamp))}</span>
+			<span className="message-author" style={{ color: wordToHexColor(author) }}>
+				{author}
+			</span>
+			<span className="message-text">
+				{text}
+				<span className="message-time">{getNaturalTimeFromDate(new Date(timestamp))}</span>
+			</span>
 		</div>
 	)
 }
