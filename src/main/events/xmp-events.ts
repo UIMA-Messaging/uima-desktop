9bcdb89476f4bbec6487ffc1fba41bf546acba21
greffgreff <greffchandler80@gmail.com>
2023-05-04 14:35:39 +0200

    Cleaup ups

--- src/main/events/xmp-events.ts
+++ src/main/events/xmp-events.ts
@@ -1,7 +1,28 @@
-import { NetworkMessage } from '../../common/types'
-import { notifyOfStatus, notifyOfError } from '../handlers/xmp-handlers'
 import { ejabberd, encryption } from '..'
+import { Invitation, Message, NetworkMessage } from '../../common/types'
+import { notifyOfStatus, notifyOfError } from '../handlers/xmp-handlers'
 import { notifyOfNewMessage } from '../handlers/message-handlers'
+import { messageTypes } from '../../common/constants'
+
+ejabberd.on('onReceived', async (type: string, encryptedMessage: NetworkMessage) => {
+	const { message: decrypted } = await encryption.decrypt(encryptedMessage)
+
+	switch (type) {
+		case messageTypes.CONTACT.INVITATION:
+			const invitation = decrypted as Invitation
+			await encryption.establishedPostExchange(invitation.user.id, invitation.postKeyBundle)
+			break
+		case messageTypes.CHANNELS.MESSAGE:
+			const channelMessage = decrypted as { channelId: string; message: Message }
+			notifyOfNewMessage(channelMessage.channelId, channelMessage.message)
+			break
+		case messageTypes.GROUP.INVITATION:
+			break
+		default:
+			console.log('Unhandled message:', decrypted)
+			break
+	}
+})
 
 ejabberd.on('onConnected', () => {
 	console.log('User now connected to chating server.')
@@ -11,11 +32,6 @@ ejabberd.on('onConnected', () => {
 ejabberd.on('onDisconnected', () => {
 	console.log('User disconnected from chating server.')
 	notifyOfStatus(false)
-})
-
-ejabberd.on('onMessageReceived', async (encryptedMessage: NetworkMessage) => {
-	const { message } = await encryption.decrypt(encryptedMessage)
-	// notifyOfNewMessage(message)
 })
 
 ejabberd.on('onError', (error: string) => {
