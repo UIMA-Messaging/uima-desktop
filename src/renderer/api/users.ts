c1c4f674c16a86fca980901f76c3546d7490c765
greffgreff <greffchandler80@gmail.com>
2023-05-13 15:32:56 +0200

    Removed search by user

--- src/renderer/api/users.ts
+++ src/renderer/api/users.ts
@@ -4,7 +4,6 @@ import { AxiosError } from 'axios'
 import { SearchResults } from '../../common/types'
 
 const searchUserBaseUrl = process.env.IDENTITY_SERVICE_BASE_URL + '/users/search/'
-const searchIdBaseUrl = process.env.IDENTITY_SERVICE_BASE_URL + '/users/username/'
 
 export async function searchUserByQuery(query: string, count: number = 10, offset: number = 0): Promise<SearchResults<User>> {
 	try {
@@ -21,28 +20,6 @@ export async function searchUserByQuery(query: string, count: number = 10, offse
 					throw Error('Could not search users as there was an internal server error.')
 				default:
 					throw Error('Could not search users.')
-			}
-		} else {
-			throw Error('Could not connect to identity services')
-		}
-	}
-}
-
-export async function searchUserByUsername(username: string): Promise<User> {
-	try {
-		const res = await axios.get(searchIdBaseUrl + username)
-		return res.data
-	} catch (error) {
-		if (error instanceof AxiosError) {
-			switch (error?.response?.status) {
-				case 404:
-					throw Error(`User not found.`)
-				case 401:
-					throw Error(`Client is not authorised to search user.`)
-				case 500:
-					throw Error('Could not search user as there was an internal server error.')
-				default:
-					throw Error('Could not search user.')
 			}
 		} else {
 			throw Error('Could not connect to identity services')
