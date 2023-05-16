1934be15ce3d09ab511ab99304f2f0680d16d500
greffgreff <greffchandler80@gmail.com>
2023-05-16 17:44:33 +0200

    Fixed event params

--- src/main/events/xmp-events.ts
+++ src/main/events/xmp-events.ts
@@ -4,11 +4,13 @@ import { notifyOfStatus, notifyOfError } from '../handlers/xmp-handlers'
 import { notifyOfNewMessage } from '../handlers/message-handlers'
 import { messageTypes } from '../../common/constants'
 
-ejabberd.on('onReceived', async (type: string, networkMessage: NetworkMessage) => {
+ejabberd.on('onReceived', async (type: string, content: any) => {
 	try {
+		console.log(content)
+
 		switch (type) {
 			case messageTypes.CONTACT.INVITATION:
-				const { user, postKeyBundle } = networkMessage.content as unknown as Invitation
+				const { user, postKeyBundle } = content as Invitation
 
 				const { fingerprint } = await encryption.establishedPostExchange(user.id, postKeyBundle)
 
@@ -18,7 +20,7 @@ ejabberd.on('onReceived', async (type: string, networkMessage: NetworkMessage) =
 				console.log('created user')
 				break
 			case messageTypes.CHANNELS.MESSAGE:
-				const { message: decrypted } = await encryption.decrypt(networkMessage)
+				const { message: decrypted } = await encryption.decrypt(content)
 
 				const channelMessage = decrypted as { channelId: string; message: Message }
 				notifyOfNewMessage(channelMessage.channelId, channelMessage.message)
@@ -32,6 +34,7 @@ ejabberd.on('onReceived', async (type: string, networkMessage: NetworkMessage) =
 		}
 	} catch (error) {
 		console.log('Could not handle message received from XMP host:', error.message)
+		console.log(error)
 	}
 })
 
